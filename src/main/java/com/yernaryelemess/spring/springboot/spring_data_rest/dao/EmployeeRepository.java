2606fd6adaa28d20faed4c914810e2d81d23474e
Yernar Yelemess <yernaryelemess@gmail.com>
2023-11-22 23:43:50 +0600

    first commit

---
+++ src/main/java/com/yernaryelemess/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.yernaryelemess.spring.springboot.spring_data_jpa.dao;
+
+
+
+
+import com.yernaryelemess.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByName(String name);
+}
